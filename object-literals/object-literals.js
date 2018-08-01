ea00585560265bcefa8bff9353b9daa0f952cdac
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:11:07 +0300

    javascript

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
 // Create your 'me' object literal here!
+var me= {
+	firstname: "Raheeq",
+	lastname: "Darawsha",
+	favoritefoods:["water","watermelon"],
+	age: 16
+}
+var myJSON= JSON.stringify(me);
+document.write(myJSON)
